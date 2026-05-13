7dd6a698ad68c2ee59eb26fcd01297f46c60c096
lakshit-ukani <lakshit.u@turing.com>
2026-05-13 19:23:26 +0530

    changes

---
+++ sync-upstream.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+UPSTREAM_URL="https://github.com/gitroomhq/postiz-docs.git"
+REPO_NAME="postiz-docs"
+REPO_DIR="$(dirname "$0")"
+
+source "$(dirname "$0")/../sync-upstream-common.sh"
